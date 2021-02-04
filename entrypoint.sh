3b8bbf547a959317cb2bfc6c276c6587ba11356a
unknown <stevenmi@live.de>
2021-02-04 21:24:57 +0100

    adjust clone link, add logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,42 +8,43 @@ FORCE=$5
 DIRECTORY=$6
 IGNORE=$7
 
+echo "setup git options";
 _GIT_OPTION=''
-
-echo "Push to branch $BRANCH";
 if ${INPUT_FORCE}; then
     _GIT_OPTION='${_GIT_OPTION} --force'
 fi
 
 # SETUP SOURCE REPOSITORY
-## get current directory
+echo "get current directory"
 SOURCE_REPOSITORY=$PWD
+
+echo "switch to parent directory"
 cd ..
 
 # SETUP TARGET REPOSITORY
-## clone target repository
-git clone "https://${OWNER}:${TOKEN}@github.com/${REPOSITORY}.git"
+echo "clone target repository"
+git clone "https://${TOKEN}@github.com/${OWNER}/${REPOSITORY}.git"
 cd ${REPOSITORY}
 
-## get latest changes and change branches
+echo "get latest changes and change branches"
 git fetch origin
 git checkout ${BRANCH}
 
-## create target folder if not exist
+echo "create target folder if not exist"
 mkdir -p ${DIRECTORY}
 
 # COPY FILES
-## create rsync exclude option
+echo "create rsync exclude option"
 _EXCLUDE_OPTION="--exclude .git"
 for i in ${IGNORE//,/ }
 do
     _EXCLUDE_OPTION="${_EXCLUDE_OPTION} --exclude $i"
 done
 
-## copy files
+echo "copy files"
 rsync -a ${_EXCLUDE_OPTION} ${SOURCE_REPOSITORY} "./${DIRECTORY}"
 
-# create commit
+echo "create commit"
 git add -A
 git commit -m "Latest changes from ${OWNER}/${REPOSITORY}"
 git push origin ${BRANCH}
